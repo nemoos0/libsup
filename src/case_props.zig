4a163072d53aa6639ed61b0e7678edb242786f55
Matteo Romano <52749992+nemoos0@users.noreply.github.com>
2025-05-17 15:02:35 +0200

    feat: add case properties functions

--- src/case_props.zig
+++ src/case_props.zig
@@ -3,13 +3,45 @@ const table = @import("case_props_table");
 
 const CaseProps = table.CaseProps;
 
-fn get(code: u21) CaseProps {
+pub fn get(code: u21) CaseProps {
     std.debug.assert(code < 0x110000);
     return table.s2[
         @as(u32, @intCast(
             table.s1[code / table.bs],
         )) * table.bs + code % table.bs
     ];
+}
+
+pub fn isUppercase(code: u21) bool {
+    return get(code).uppercase;
+}
+
+pub fn isLowercase(code: u21) bool {
+    return get(code).lowercase;
+}
+
+pub fn isCased(code: u21) bool {
+    return get(code).cased;
+}
+
+pub fn isCaseIgnorable(code: u21) bool {
+    return get(code).case_ignorable;
+}
+
+pub fn changesWhenLowercased(code: u21) bool {
+    return get(code).changes_when_lowercased;
+}
+
+pub fn changesWhenTitlecased(code: u21) bool {
+    return get(code).changes_when_titlecased;
+}
+
+pub fn changesWhenUppercased(code: u21) bool {
+    return get(code).changes_when_uppercased;
+}
+
+pub fn changesWhenCasefolded(code: u21) bool {
+    return get(code).changes_when_casefolded;
 }
 
 pub fn changesWhenCasemapped(code: u21) bool {
