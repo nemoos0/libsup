267910c39699e3badf776a31db12c7b494edae5e
Matteo Romano <52749992+nemoos0@users.noreply.github.com>
2025-05-17 19:15:37 +0200

    feat: case folder iterator

--- src/case_folding.zig
+++ src/case_folding.zig
@@ -1,5 +1,39 @@
 const std = @import("std");
 const table = @import("case_folding_table");
+const codepoint = @import("codepoint");
+
+const Folder = struct {
+    source: codepoint.Iterator,
+    codes: []const u21 = &.{},
+    pos: u8,
+
+    pub fn nextSimple(fold: Folder) !?u21 {
+        if (try fold.source.next()) |cp| {
+            return getSimple(cp.code);
+        }
+
+        return null;
+    }
+
+    pub fn nextFull(fold: Folder) !?u21 {
+        if (fold.pos < fold.codes.len) {
+            defer fold.pos += 1;
+            return fold.codes[fold.pos];
+        }
+
+        if (try fold.source.next()) |cp| {
+            if (getFull(cp.code)) |full| {
+                fold.source = full;
+                fold.pos = 1;
+                return fold.source[0];
+            } else {
+                return cp.code;
+            }
+        }
+
+        return null;
+    }
+};
 
 pub fn getFull(code: u21) ?[]const u21 {
     std.debug.assert(code < 0x110000);
@@ -21,7 +55,7 @@ test "getFull" {
     try std.testing.expectEqualSlices(u21, &[_]u21{ 0x1f00, 0x03b9 }, getFull(0x1f88).?);
 }
 
-pub fn getSimple(code: u21) ?u21 {
+pub fn getSimple(code: u21) u21 {
     std.debug.assert(code < 0x110000);
 
     const high_bits = code / table.bs;
@@ -30,7 +64,7 @@ pub fn getSimple(code: u21) ?u21 {
     const idx: u32 = @as(u32, @intCast(table.s1[high_bits])) * table.bs + low_bits;
 
     const len = table.s2_len[idx];
-    if (len == 0) return null;
+    if (len == 0) return code;
 
     const off = table.s2_off[idx];
     return table.codes[off + len * @intFromBool(len > 1)];
